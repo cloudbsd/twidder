c263f4ac2a971a7ed6e7cc6bbd2919f8a450397c
Qi Li <cloudbsd@gmail.com>
2012-03-17 21:46:23 +0800

    import initially created twidder project.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Twidder::Application.config.secret_token = '67cb4a311afa095063b6424946c4e3be9e73c4ec929dbab1492d023b74a89b01f48ea31e82fdb771fd4db65f23ad00318960ca74337f657d67f00c5d29c7cf44'
