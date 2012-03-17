c300276f331ff7002a242997859a8497e7856b36
Qi Li <cloudbsd@gmail.com>
2012-03-18 03:07:18 +0800

    add home controller and index action.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Twidder::Application.routes.draw do
   devise_for :users
 
+  get "home/index"
+  root :to => 'home#index'
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
