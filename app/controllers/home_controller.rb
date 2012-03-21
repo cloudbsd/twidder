38f176a93faba0e64d16a4debdaf40a004a7f5e8
Qi Li <cloudbsd@gmail.com>
2012-03-22 00:42:47 +0800

    support zh-CN for home/index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!
+# before_filter :authenticate_user!
 
   def index
+    flash[:notice] = t(:hello_flash, scope: 'home.index')
   end
 end
