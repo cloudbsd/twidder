d0afa0ad4eafd5117159192608d1e7e750584e30
Qi Li <cloudbsd@gmail.com>
2012-03-30 00:47:05 +0800

    add I18n to test the error message.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -5,10 +5,20 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # end
 
+  # EXAMPLE:
+  #
+  # ActiveModel handles most of the errors, so the error messages are in errors.messages. The only exception is taken, because the uniqueness validation is ActiveRecord specific.
+  #
+  # So you'll need:
+  #
+  # assert_equal I18n.translate('errors.messages.too_short', :count=>10), product.errors[:title].join('; ')
+  #
+
   test "invalid with empty attributes" do
     user = User.new
     assert user.invalid?
     assert user.errors[:email].any?
-  # assert_equal I18n.translate('activerecord.errors.messages.empty'), user.errors[:name].join('; ')
+    assert_equal I18n.translate('errors.messages.blank'), user.errors[:email].join('; ')
+    assert_equal I18n.translate('errors.messages.blank'), user.errors[:name].join('; ')
   end
 end
