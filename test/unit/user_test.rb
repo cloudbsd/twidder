628dc14a1c78697f6c0ba78974af6cb1d4ca43e2
Qi Li <cloudbsd@gmail.com>
2012-03-30 00:29:50 +0800

    add group model.

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -4,4 +4,11 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "invalid with empty attributes" do
+    user = User.new
+    assert user.invalid?
+    assert user.errors[:email].any?
+  # assert_equal I18n.translate('activerecord.errors.messages.empty'), user.errors[:name].join('; ')
+  end
 end
