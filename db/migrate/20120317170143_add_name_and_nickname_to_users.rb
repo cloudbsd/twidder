e14f32964376bf0c7a7115cf9c3fe38d7594836f
Qi Li <cloudbsd@gmail.com>
2012-04-07 23:54:14 +0800

    use change_table instead of change when add new column.

--- db/migrate/20120317170143_add_name_and_nickname_to_users.rb
+++ db/migrate/20120317170143_add_name_and_nickname_to_users.rb
@@ -1,9 +1,17 @@
 class AddNameAndNicknameToUsers < ActiveRecord::Migration
+# def change
+#   add_column :users, :name, :string
+#   add_index :users, :name, :unique => true
+
+#   add_column :users, :nickname, :string
+
+# end
+
   def change
-    add_column :users, :name, :string
-    add_index :users, :name, :unique => true
-
-    add_column :users, :nickname, :string
-
+    change_table :users do |t|
+      t.string :name,              :null => false, :default => ""
+      t.string :nickname,          :null => false, :default => ""
+    end
+    add_index :users, :name,       :unique => true
   end
 end
