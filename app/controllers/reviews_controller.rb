c6a2610d9e76476fa3a6afc96a41c22fc2112f5c
Qi Li <cloudbsd@gmail.com>
2012-05-03 02:13:31 +0800

    destory review feature works.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,14 +2,14 @@ class ReviewsController < ApplicationController
   # POST /projects/1/reviews
   # POST /projects/1/reviews.json
   def create
-    @project = Post.find(params[:project_id])
+    @project = Project.find(params[:project_id])
     @review = @project.reviews.build(params[:review])
     @review.user = current_user
 
     respond_to do |format|
       if @project.save
         format.html { redirect_to @project, notice: 'Review was successfully created.' }
-        format.js   { @current_review = @review }
+        format.js
         format.json { render json: @project, status: :created, location: @project }
       else
         format.html { render action: "new" }
@@ -21,12 +21,13 @@ class ReviewsController < ApplicationController
   # DELETE /projects/1/reviews/1
   # DELETE /projects/1/reviews/1.json
   def destroy
-    @project = Post.find(params[:project_id])
+    @project = Project.find(params[:project_id])
     @review = @project.reviews.find(params[:id])
+    paths = @review.file
     @review.destroy
 
     respond_to do |format|
-      format.html { redirect_to @project }
+      format.html { redirect_to blob_project_path(@project, 'blob', paths) }
       format.js
       format.json { head :no_content }
     end
