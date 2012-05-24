153e998b75857db9840c33b9eb564b75ddf4403a
Qi Li <cloudbsd@gmail.com>
2012-05-24 22:45:21 +0800

    ajax work for sort, paginate and search.

--- app/assets/javascripts/pagination.js
+++ app/assets/javascripts/pagination.js
@@ -7,3 +7,14 @@ $(function() {
   });
 });
 */
+
+$(function() {
+  $("#posts th a, #posts .pagination a").live("click", function() {
+    $.getScript(this.href);
+    return false;
+  });
+  $("#posts_search input").keyup(function() {
+    $.get($("#posts_search").attr("action"), $("#posts_search").serialize(), null, "script");
+    return false;
+  });
+});
